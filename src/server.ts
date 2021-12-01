cd91e87bd1d99f62ebdb08a33130ca19f7469fde
Imobpower 10 <imobpower10@Imobpowers-Mini.Imobpowernetwork>
2021-12-01 11:34:24 -0300

    nv 04 - 01 - 04 infra

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,34 @@
-import express, { Request, Response, NextFunction } from 'express';
 import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
 import routes from './routes';
 import './database';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => { });
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.log(err);
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
