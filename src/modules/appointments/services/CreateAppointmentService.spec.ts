0f2e547f6f9d201ae8a0f8f1d40008b3d18ec4d4
nicaobueno <nicaobueno@gmail.com>
2021-12-17 00:28:31 -0300

    nv 04 - DDD - Jest

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,24 @@
+import CreateAppointmentService from './CreateAppointmentService';
+import FakeAppointmentsRepository from '../repositories/fake/fakeAppointmentsRepository';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123456',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123456');
+  });
+
+  //   it('should not be able to create a new appointment on the same time', () => {
+  //     expect('');
+  //   });
+});
